0142e76e7051b49a4cb9a033d14c39b603ac2ab0
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2019-10-03 14:20:13 +0300

    layout Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import dialogs from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>dialog</div>
+        <div className={dialogs.dialogs}>
+            <div className={dialogs.dialogsItem}>
+                <div className={dialogs.dialog + ' ' + dialogs.active}>Tolya</div>
+                <div className={dialogs.dialog}>Pasha</div>
+                <div className={dialogs.dialog}>Andrey</div>
+                <div className={dialogs.dialog}>Igor</div>
+                <div className={dialogs.dialog}>Kirill</div>
+            </div>
+            <div className={dialogs.messages}>
+                <div className={dialogs.message}>Hi</div>
+                <div className={dialogs.message}>How are you</div>
+                <div className={dialogs.message}>you</div>
+            </div>
+        </div>
     )
-
 };
 
 export default Dialogs;
