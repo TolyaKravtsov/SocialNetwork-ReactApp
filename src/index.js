6f45d45a5f7aab38105c7fc47f0e3081ef9331c6
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2019-10-15 12:25:45 +0300

    refactor BLL,delete render.js; add observer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from "react-router-dom";
+import state, {addPost, subscribe, updateNewPostText} from "./redux/state";
+
+let rerenderEntireTree = (state) => {
+
+    ReactDOM.render(
+        <Router>
+            <App state={state}
+                 addPost={addPost}
+                 updateNewPostText={updateNewPostText}/>,
+        </Router>,
+        document.getElementById('root'));
+};
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
+
+serviceWorker.unregister();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log('priv');
+};
 
 let state = {
 
@@ -34,7 +36,7 @@ let state = {
 };
 
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText
@@ -44,8 +46,13 @@ export let addPost = () => {
 };
 
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+};
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
+
 };
 export default state
