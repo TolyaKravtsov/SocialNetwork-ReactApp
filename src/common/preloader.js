53565c22962c2a5ef99fe2a998273b245c2f77a7
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2019-11-04 18:25:08 +0300

    Preloader

---
+++ src/common/preloader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import preloader from "./Rolling-2s-200px.svg"
+
+let preloaderComponent = (props) => {
+    return (
+        <div>
+        <img src={preloader}/>
+        </div>
+    )
+};
+
+export default preloaderComponent;
