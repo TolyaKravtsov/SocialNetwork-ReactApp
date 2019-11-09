5ecf6be5905175578066dd398a8336b7f79e7a5c
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2019-11-10 01:18:14 +0300

    follow-unfollow api

--- src/components/Users/FindUsers.js
+++ src/components/Users/FindUsers.js
@@ -1,8 +1,9 @@
 import React from "react";
 import findUsers from "./findUsers.module.css";
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
-let Users = (props) => {
+let Users = props => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
@@ -11,57 +12,96 @@ let Users = (props) => {
 
     return (
         <div>
-            {
-                pages.map(pages => {
-                    return <span className={props.currentPage === pages && findUsers.selectedPage}
-                                 onClick={() => {
-                                     props.onPageChanged(pages)
-                                 }}>{pages}</span>
-                })
-            }
-            {
-                props.users.map(users => <div key={users.id}>
-                <span>
-                    <div>
-                        <NavLink to={'/profile/' + users.id}>
-                        <img className={findUsers.img}
-                             src={users.photos.small != null ? users.photos.small : "https://cutt.ly/denvfka"}
-                             alt="photo"/>
-                        </NavLink>
-                    </div>
-                     <div>
-                        {
-                            users.followed
-                                ? <button onClick={() => {
-                                    props.unfollow(users.id)
-                                }}>Unfollow</button>
-                                : <button onClick={() => {
-                                    props.follow(users.id)
-                                }}>Follow</button>
-                        }
+            {pages.map(pages => {
+                return (
+                    <span
+                        className={props.currentPage === pages && findUsers.selectedPage}
+                        onClick={() => {
+                            props.onPageChanged(pages);
+                        }}
+                    >
+            {pages}
+          </span>
+                );
+            })}
+            {props.users.map(users => (
+                <div key={users.id}>
+          <span>
+            <div>
+              <NavLink to={"/profile/" + users.id}>
+                <img
+                    className={findUsers.img}
+                    src={
+                        users.photos.small != null
+                            ? users.photos.small
+                            : "https://cutt.ly/denvfka"
+                    }
+                    alt="photo"
+                />
+              </NavLink>
+            </div>
+            <div>
+              {users.followed ? (
+                  <button
+                      onClick={() => {
+                          axios
+                              .delete(
+                                  `https://social-network.samuraijs.com/api/1.0//follow/${users.id}`,
+                                  {
+                                      withCredentials: true, headers: {
+                                          "API-KEY": "858cdff0-21c8-4da1-ac2d-e0c7e81bba49"
+                                      }
+                                  },
+                              )
+                              .then(response => {
+                                  if (response.data.resultCode == 0) {
+                                      props.unfollow(users.id);
+                                  }
+                              });
+                      }}
+                  >
+                      Unfollow
+                  </button>
+              ) : (
+                  <button
+                      onClick={() => {
+                          axios
+                              .post(
+                                  `https://social-network.samuraijs.com/api/1.0//follow/${users.id}`,
+                                  {},
+                                  {
+                                      withCredentials: true,
+                                      headers: {
+                                          "API-KEY": "858cdff0-21c8-4da1-ac2d-e0c7e81bba49"
+                                      }
+                                  }
+                              )
+                              .then(response => {
+                                  if (response.data.resultCode == 0) {
+                                      props.follow(users.id);
+                                  }
+                              });
+                      }}
+                  >
+                      Follow
+                  </button>
+              )}
+            </div>
+          </span>
+                    <span>
+            <span>
+              <div>{users.name}</div>
+              <div>{users.id}</div>
+            </span>
+            <span>
+              <div>{""}</div>
+              <div>{""}</div>
+            </span>
+          </span>
                 </div>
-                </span>
-                    <span>
-                    <span>
-                        <div>
-                            {users.name}
-                        </div>
-                        <div>
-                            {users.id}
-                        </div>
-                    </span>
-                    <span>
-                        <div>
-                            {""}
-                        </div>
-                        <div>
-                            {""}
-                        </div>
-                    </span>
-                </span>
-                </div>)}
+            ))}
         </div>
-    )
+    );
 };
 
 export default Users;
--- src/components/Users/findUsersContainer.jsx
+++ src/components/Users/findUsersContainer.jsx
@@ -1,65 +1,84 @@
 import React from "react";
 import {
-    follow,
-    preloaderTurned,
-    setCurrentPage,
-    setUsers,
-    setUsersTotalCount,
-    unfollow
+  follow,
+  preloaderTurned,
+  setCurrentPage,
+  setUsers,
+  setUsersTotalCount,
+  unfollow
 } from "../../redux/reducer/findUsersPageReducer";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import * as axios from "axios";
 import Users from "./FindUsers";
 import PreloaderComponent from "../../common/preloader";
 
 class usersContainer extends React.Component {
-    componentDidMount() {
-        this.props.preloaderTurned(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.preloaderTurned(false);
-            this.props.setUsers(response.data.items);
-            this.props.setUsersTotalCount(response.data.totalCount)
-        });
-    }
+  componentDidMount() {
+    this.props.preloaderTurned(true);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{
+            withCredentials: true
+          }
+      )
+      .then(response => {
+        this.props.preloaderTurned(false);
+        this.props.setUsers(response.data.items);
+        this.props.setUsersTotalCount(response.data.totalCount);
+      });
+  }
 
-    onPageChanged = (pageNumber) => {
-        this.props.preloaderTurned(true);
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.preloaderTurned(false);
-            this.props.setUsers(response.data.items)
-        });
-    };
+  onPageChanged = pageNumber => {
+    this.props.preloaderTurned(true);
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,{
+            withCredentials: true
+          }
+      )
+      .then(response => {
+        this.props.preloaderTurned(false);
+        this.props.setUsers(response.data.items);
+      });
+  };
 
-
-    render() {
-        return (
-            <>
-                {this.props.inProgress ? <PreloaderComponent/> : null}
-                <Users totalUsersCount={this.props.totalUsersCount}
-                       pageSize={this.props.pageSize}
-                       currentPage={this.props.currentPage}
-                       users={this.props.users}
-                       onPageChanged={this.onPageChanged}
-                       unfollow={this.props.unfollow}
-                       follow={this.props.follow}
-                />
-
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        {this.props.inProgress ? <PreloaderComponent /> : null}
+        <Users
+          totalUsersCount={this.props.totalUsersCount}
+          pageSize={this.props.pageSize}
+          currentPage={this.props.currentPage}
+          users={this.props.users}
+          onPageChanged={this.onPageChanged}
+          unfollow={this.props.unfollow}
+          follow={this.props.follow}
+        />
+      </>
+    );
+  }
 }
 
-let mapStateToProps = (state) => {
-    return {
-        users: state.findUsersPage.users,
-        pageSize: state.findUsersPage.pageSize,
-        totalUsersCount: state.findUsersPage.totalUsersCount,
-        currentPage: state.findUsersPage.currentPage,
-        inProgress: state.findUsersPage.inProgress,
-    }
+let mapStateToProps = state => {
+  return {
+    users: state.findUsersPage.users,
+    pageSize: state.findUsersPage.pageSize,
+    totalUsersCount: state.findUsersPage.totalUsersCount,
+    currentPage: state.findUsersPage.currentPage,
+    inProgress: state.findUsersPage.inProgress
+  };
 };
 
-export default connect(mapStateToProps, {follow, unfollow, setUsers, setCurrentPage, setUsersTotalCount, preloaderTurned,})(usersContainer);
-
-
+export default connect(
+  mapStateToProps,
+  {
+    follow,
+    unfollow,
+    setUsers,
+    setCurrentPage,
+    setUsersTotalCount,
+    preloaderTurned
+  }
+)(usersContainer);
