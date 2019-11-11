f10c49939a85643826a0fa4cafdde58f09503f06
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2019-11-11 17:02:28 +0300

    DAL,axios.create

--- src/components/Users/findUsersContainer.jsx
+++ src/components/Users/findUsersContainer.jsx
@@ -1,84 +1,75 @@
 import React from "react";
 import {
-  follow,
-  preloaderTurned,
-  setCurrentPage,
-  setUsers,
-  setUsersTotalCount,
-  unfollow
+    follow,
+    preloaderTurned,
+    setCurrentPage,
+    setUsers,
+    setUsersTotalCount,
+    unfollow
 } from "../../redux/reducer/findUsersPageReducer";
-import { connect } from "react-redux";
-import * as axios from "axios";
+import {connect} from "react-redux";
 import Users from "./FindUsers";
 import PreloaderComponent from "../../common/preloader";
+import {getUsers, usersAPI} from "../../api/api";
 
 class usersContainer extends React.Component {
-  componentDidMount() {
-    this.props.preloaderTurned(true);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{
-            withCredentials: true
-          }
-      )
-      .then(response => {
-        this.props.preloaderTurned(false);
-        this.props.setUsers(response.data.items);
-        this.props.setUsersTotalCount(response.data.totalCount);
-      });
-  }
+    componentDidMount() {
+        this.props.preloaderTurned(true);
 
-  onPageChanged = pageNumber => {
-    this.props.preloaderTurned(true);
-    this.props.setCurrentPage(pageNumber);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,{
-            withCredentials: true
-          }
-      )
-      .then(response => {
-        this.props.preloaderTurned(false);
-        this.props.setUsers(response.data.items);
-      });
-  };
+      usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
+            .then(data => {
+                this.props.preloaderTurned(false);
+                this.props.setUsers(data.items);
+                this.props.setUsersTotalCount(data.totalCount);
+            });
+    }
 
-  render() {
-    return (
-      <>
-        {this.props.inProgress ? <PreloaderComponent /> : null}
-        <Users
-          totalUsersCount={this.props.totalUsersCount}
-          pageSize={this.props.pageSize}
-          currentPage={this.props.currentPage}
-          users={this.props.users}
-          onPageChanged={this.onPageChanged}
-          unfollow={this.props.unfollow}
-          follow={this.props.follow}
-        />
-      </>
-    );
-  }
+    onPageChanged = pageNumber => {
+        this.props.preloaderTurned(true);
+        this.props.setCurrentPage(pageNumber);
+        usersAPI.getUsers(pageNumber, this.props.pageSize)
+            .then(data => {
+                this.props.preloaderTurned(false);
+                this.props.setUsers(data.items);
+            });
+    };
+
+    render() {
+        return (
+            <>
+                {this.props.inProgress ? <PreloaderComponent/> : null}
+                <Users
+                    totalUsersCount={this.props.totalUsersCount}
+                    pageSize={this.props.pageSize}
+                    currentPage={this.props.currentPage}
+                    users={this.props.users}
+                    onPageChanged={this.onPageChanged}
+                    unfollow={this.props.unfollow}
+                    follow={this.props.follow}
+                />
+            </>
+        );
+    }
 }
 
 let mapStateToProps = state => {
-  return {
-    users: state.findUsersPage.users,
-    pageSize: state.findUsersPage.pageSize,
-    totalUsersCount: state.findUsersPage.totalUsersCount,
-    currentPage: state.findUsersPage.currentPage,
-    inProgress: state.findUsersPage.inProgress
-  };
+    return {
+        users: state.findUsersPage.users,
+        pageSize: state.findUsersPage.pageSize,
+        totalUsersCount: state.findUsersPage.totalUsersCount,
+        currentPage: state.findUsersPage.currentPage,
+        inProgress: state.findUsersPage.inProgress
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  {
-    follow,
-    unfollow,
-    setUsers,
-    setCurrentPage,
-    setUsersTotalCount,
-    preloaderTurned
-  }
+    mapStateToProps,
+    {
+        follow,
+        unfollow,
+        setUsers,
+        setCurrentPage,
+        setUsersTotalCount,
+        preloaderTurned
+    }
 )(usersContainer);
