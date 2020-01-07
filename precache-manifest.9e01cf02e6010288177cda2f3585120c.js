d1e59459c920335c066965fadc17e2ad7cebd552
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2020-01-07 19:07:33 +0300

    Updates

---
+++ precache-manifest.9e01cf02e6010288177cda2f3585120c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9135f84b53f99c01c81fe9ae2139bd",
+    "url": "/SocialNetwork-ReactApp/index.html"
+  },
+  {
+    "revision": "f0e3cdc11cb0be08f40a",
+    "url": "/SocialNetwork-ReactApp/static/css/3.6603fa80.chunk.css"
+  },
+  {
+    "revision": "3b65884ea3fa832715e2",
+    "url": "/SocialNetwork-ReactApp/static/css/4.827deb10.chunk.css"
+  },
+  {
+    "revision": "4c2765f1f17ca1590e90",
+    "url": "/SocialNetwork-ReactApp/static/css/main.32ed165c.chunk.css"
+  },
+  {
+    "revision": "3f38406c1fc6fc1bd068",
+    "url": "/SocialNetwork-ReactApp/static/js/2.53706384.chunk.js"
+  },
+  {
+    "revision": "f0e3cdc11cb0be08f40a",
+    "url": "/SocialNetwork-ReactApp/static/js/3.cb18ef5a.chunk.js"
+  },
+  {
+    "revision": "3b65884ea3fa832715e2",
+    "url": "/SocialNetwork-ReactApp/static/js/4.d97587dc.chunk.js"
+  },
+  {
+    "revision": "4c2765f1f17ca1590e90",
+    "url": "/SocialNetwork-ReactApp/static/js/main.4f6b9bb6.chunk.js"
+  },
+  {
+    "revision": "f9ff484518491062cb80",
+    "url": "/SocialNetwork-ReactApp/static/js/runtime~main.cfca1ba8.js"
+  },
+  {
+    "revision": "fe5a47a9b5151ad8b04d032cf6ab60b2",
+    "url": "/SocialNetwork-ReactApp/static/media/Rolling-2s-200px.fe5a47a9.svg"
+  }
+]);
