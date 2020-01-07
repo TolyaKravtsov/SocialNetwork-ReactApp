b87553570e5549db8ea70014b869634a5b6f47b0
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2020-01-07 17:01:06 +0300

    Updates

---
+++ precache-manifest.406b74d8495eccd47a7d1917cfd66663.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847ab1a75fce4a6a543ce46a7fdfd526",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "f0e3cdc11cb0be08f40a",
+    "url": "/react-app/static/css/3.6603fa80.chunk.css"
+  },
+  {
+    "revision": "3b65884ea3fa832715e2",
+    "url": "/react-app/static/css/4.827deb10.chunk.css"
+  },
+  {
+    "revision": "cbb8418d44ce1ddf4e03",
+    "url": "/react-app/static/css/main.32ed165c.chunk.css"
+  },
+  {
+    "revision": "3f38406c1fc6fc1bd068",
+    "url": "/react-app/static/js/2.53706384.chunk.js"
+  },
+  {
+    "revision": "f0e3cdc11cb0be08f40a",
+    "url": "/react-app/static/js/3.cb18ef5a.chunk.js"
+  },
+  {
+    "revision": "3b65884ea3fa832715e2",
+    "url": "/react-app/static/js/4.d97587dc.chunk.js"
+  },
+  {
+    "revision": "cbb8418d44ce1ddf4e03",
+    "url": "/react-app/static/js/main.3b2fb3c3.chunk.js"
+  },
+  {
+    "revision": "820747e1b15e51ce61b5",
+    "url": "/react-app/static/js/runtime~main.2419f70d.js"
+  },
+  {
+    "revision": "fe5a47a9b5151ad8b04d032cf6ab60b2",
+    "url": "/react-app/static/media/Rolling-2s-200px.fe5a47a9.svg"
+  }
+]);
